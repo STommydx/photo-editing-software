e1ac78b41172af07d1b0d02be0fd3f3373db11a4
STommydx <ttoo74@gmail.com>
2018-11-25 15:29:58 +0800

    Pixelize

--- imageutil.cpp
+++ imageutil.cpp
@@ -37,3 +37,26 @@ QImage ImageUtil::sharpen(const QImage &img, int size)
 {
     return ImageKernel::sharpen(size).convolution(img);
 }
+
+QImage ImageUtil::pixelize(const QImage &img, int size)
+{
+    QImage newImg{img};
+    for (int i=0;i<img.height();i+=size) for (int j=0;j<img.width();j+=size) {
+        int rsum = 0, gsum = 0, bsum = 0, asum = 0;
+        int normFact = 0;
+        for (int dx=0;dx<size;dx++) for (int dy=0;dy<size;dy++) {
+            int nx = i + dx, ny = j + dy;
+            if (nx < 0 || nx >= img.height() || ny < 0 || ny >= img.width()) continue;
+            normFact++;
+            QRgb pix = *getPixel(img, nx, ny);
+            rsum += qRed(pix); gsum += qGreen(pix); bsum += qBlue(pix); asum += qAlpha(pix);
+        }
+        QRgb res = qRgba(rsum / normFact, gsum / normFact, bsum / normFact, asum / normFact);
+        for (int dx=0;dx<size;dx++) for (int dy=0;dy<size;dy++) {
+            int nx = i + dx, ny = j + dy;
+            if (nx < 0 || nx >= img.height() || ny < 0 || ny >= img.width()) continue;
+            *getPixel(newImg, i+dx, j+dy) = res;
+        }
+    }
+    return newImg;
+}
--- imageutil.h
+++ imageutil.h
@@ -14,6 +14,7 @@ public:
     static QImage meanBlur(const QImage &img, int size = 3);
     static QImage gaussianBlur(const QImage &img, int size = 3, double stDev = 1.0);
     static QImage sharpen(const QImage &img, int size = 2);
+    static QImage pixelize(const QImage &img, int size = 4);
 
 };
 
