70a15cbca7ed058135e9f5f992340782307797da
STommydx <ttoo74@gmail.com>
2018-12-02 18:08:27 +0800

    Grayscale filter doc

--- filter/grayscalefilter.cpp
+++ filter/grayscalefilter.cpp
@@ -1,6 +1,17 @@
+/**
+ * @class GrayscaleFilter
+ * @brief The GrayscaleFilter class represents a grayscale filter.
+ *
+ * It average out the values of all red, green and blue channels and produces a grayscale image.
+ */
+
 #include "grayscalefilter.h"
 #include "imageutil.h"
 
+/**
+ * @brief Constructs a grayscale filter with the given parent
+ * @param parent the parent @c QObject
+ */
 GrayscaleFilter::GrayscaleFilter(QObject *parent) : ImagePureFilter{parent} {}
 
 QString GrayscaleFilter::getName() const
@@ -15,7 +26,7 @@ QImage GrayscaleFilter::apply(const QImage &img) const
         emit progressUpdated(i);
         for (int j=0;j<img.width();j++) {
             QRgb pix = *ImageUtil::getPixel(img, i, j);
-            int avg = (qRed(pix) + qGreen(pix) + qBlue(pix)) / 3;
+            int avg = (qRed(pix) + qGreen(pix) + qBlue(pix)) / 3; // calculate the brightness by averaging the 3 channels
             *ImageUtil::getPixel(newImg, i, j) = qRgba(avg, avg, avg, qAlpha(pix));
         }
     }
