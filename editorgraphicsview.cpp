f021edf8ec5643011a99896484f81c1b71018945
STommydx <ttoo74@gmail.com>
2018-12-02 16:55:41 +0800

    egv doc

--- editorgraphicsview.cpp
+++ editorgraphicsview.cpp
@@ -1,11 +1,23 @@
-#include "editorgraphicsview.h"
-#include <QDebug>
+/**
+ * @class EditorGraphicsView
+ * @brief The EditorGraphicsView class provides a widget for viewing the EditorGraphicsScene.
+ */
 
+#include "editorgraphicsview.h"
+
+/**
+ * @brief Constructs a viewing widget with the given parent
+ * @param parent the parent widget
+ */
 EditorGraphicsView::EditorGraphicsView(QWidget *parent) : QGraphicsView(parent)
 {
     setInteractive(true);
 }
 
+/**
+ * @brief Handles the action when the user resize the view or its parent
+ * @param event the resize event
+ */
 void EditorGraphicsView::resizeEvent(QResizeEvent *event) {
     fitInView(scene()->sceneRect(), Qt::KeepAspectRatio);
     QGraphicsView::resizeEvent(event);
